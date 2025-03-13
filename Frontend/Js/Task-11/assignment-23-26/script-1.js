387154e7e8c8e886bae1157420477173f2953393
abdelrhman5052 <almsrybdalrhmn111@gmail.com>
2025-03-13 20:13:47 +0200

    done

---
+++ Frontend/Js/Task-11/assignment-23-26/script-1.js
@@ -0,0 +1,15 @@
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+
+console.log(1e5);
+console.log(Math.ceil(99999.2)); // 100000
+console.log(Math.floor(100000.99)); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(Math.round(100000.4)); // 100000
+console.log(Number((100000.0).toFixed(0)));
+console.log(Math.pow(10, 5));
+console.log(Number("100000"));
+console.log(parseInt("100000"));
+console.log("50000" * 2);
